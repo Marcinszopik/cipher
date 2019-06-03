bd3e3db3017fd5b12ac62d115b1b76726e87ecde
Marcin Szopik and Rochelle Roberts <marcin@>
2019-06-03 16:25:38 -0700

    capitalize first letter added

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
 var sentence = prompt("Type a sentence");
-alert("the user wrote: " + sentence);
+alert("The user wrote: " + sentence);
+
+// capitalize the first letter
+function capitalizeLetters(sentence){
+  var letter0 = sentence.charAt(0);
+  alert("first letter is : " + letter0);
+  letter0 = letter0.toUpperCase(); // now first letter is capitalized
+  var result = sentence.slice(1);
+  return letter0 + result;
+};
+
+
+alert(capitalizeLetters(sentence));
